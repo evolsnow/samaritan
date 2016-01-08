e01feb6e64a0b59f32845d4eafd61deb7a29eeb6
evolsnow <gsc1215225@gmail.com>
2016-01-08 18:33:33 +0800

    refine files

--- conn/redis.go
+++ conn/redis.go
@@ -64,3 +64,12 @@ func CacheSet(key string, value interface{}, px time.Duration) {
 	defer c.Close()
 	c.Do("SET", key, value, "PX", int64(px/time.Millisecond))
 }
+
+//
+//func CreateProduct(p *model.RawProduct) (interface{}, error) {
+//	conn := Pool.Get()
+//	defer conn.Close()
+//	return CreateProductScript.Do(conn, p.Name, p.CreateTime, p.ReviewTime, p.PublishTime, p.InterestStart, p.InterestEnd,
+//		p.Period, p.FundsAmount, p.MinPurchase, p.SalseAmount, p.InterestYear, p.RepaymentType, p.Category,
+//		p.Status, p.IntegerRequired, p.RequiredLv, p.BorrowId, p.CreateStuffId, p.ReviewStuffId)
+//}
