aecb45ec650adf049f0c91558d4b6e6c4b4c2f8b
evolsnow <gsc1215225@gmail.com>
2016-03-04 10:35:40 +0800

    update gitignore

--- model/mysql_m.go
+++ model/mysql_m.go
@@ -1,15 +1,24 @@
 package model
 
 import (
+	"database/sql"
 	"github.com/evolsnow/samaritan/conn"
 )
 
+func init() {
+	for {
+		if conn.DB != nil {
+			db = conn.DB
+			break
+		}
+	}
+}
+
+var db *sql.DB
+
 func Test() {
-	var (
-		name string
-	)
-	db := conn.DB
-	rows, err := db.Query("select name from user where pid = ?", 123)
+	var name string
+	rows, err := db.Query("select name from user where id = ?", 1)
 	if err != nil {
 		log.Fatal(err)
 	}
